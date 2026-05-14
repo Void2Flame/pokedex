38c7d7c6a4706b516e97c9516110b77316c4cc3e
Void2Flame <daniel.zinger@hotmail.com>
2026-05-14 21:51:50 +0200

    Addded width to input

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4733c5e07a4f99143e860fa6bb575b6d86a977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54a500ddc00f395ff3ce772e51e971daf468dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
