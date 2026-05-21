b405b162dad92c42a99b49f6517bb72066cb6425
Void2Flame <daniel.zinger@hotmail.com>
2026-05-21 21:42:00 +0200

    Moved all the first info into a div

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae034683805057efc495568456e408650c47338e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffd754538eb1e9b2326e43de99854c7fc14b85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
