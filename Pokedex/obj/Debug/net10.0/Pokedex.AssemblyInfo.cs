74baa22ce4a7a23e8ce2144447450d847bcf044d
Void2Flame <daniel.zinger@hotmail.com>
2026-05-24 15:17:09 +0200

    Restructed html code and added base css value

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffd754538eb1e9b2326e43de99854c7fc14b85d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b405b162dad92c42a99b49f6517bb72066cb6425")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
