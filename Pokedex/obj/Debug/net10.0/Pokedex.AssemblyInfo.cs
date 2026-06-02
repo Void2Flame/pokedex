a0c973c7aaab3cc4a59232fc25254d54256dcddf
Void2Flame <daniel.zinger@hotmail.com>
2026-06-02 18:46:30 +0200

    Have added styling to types and added images

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bef2e529c325e0dd997ea367370e2144215976")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde57292f2cb14777cec5b03cd2cdb6f29fe9b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
