15ad61317d8b57b934e578d661707fc4d53b987f
Void2Flame <daniel.zinger@hotmail.com>
2026-06-02 19:50:00 +0200

    Fixed stash error

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b405b162dad92c42a99b49f6517bb72066cb6425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c02ba3d1dcfca2ed03629c5ca7425a95dddfea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
