5967a0be3b88b067e95e9580bda78d87b7e53b46
Void2Flame <daniel.zinger@hotmail.com>
2026-06-06 16:40:46 +0200

    Removed fixed posistion on container and fixed error text

--- Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net10.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20b4a6e800bf6aba37c1b89ba6c0b3f77a1c6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212425b22710c8c9bcd557251a116c8b905d3983")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
